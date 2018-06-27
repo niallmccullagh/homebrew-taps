fef9f2fd5db4fd923017b9e2c7609fc1618e6ec6
Niall McCullagh <niall.mccullagh@gmail.com>
2018-06-27 10:56:15 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,25 @@
+# homebrew-taps
+
+This is used to deploy tooling binaries into OSX machines via the Homebrew package manager.
+
+## Requirements
+
+* MacOS
+* [Homebrew](https://brew.sh/)
+* Basic terminal (command-line) knowledge
+
+## Installation
+
+The first thing to do is install the tap by opening a terminal window
+and typing this:
+
+```bash
+brew tap niallmccullagh/homebrew-taps
+```
+
+Then install any formula you like:
+
+```bash
+brew install heptio-authenticator-aws
+```
+
---
+++ heptio-authenticator-aws.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class HeptioAuthenticatorAws < Formula
+  homepage 'https://github.com/heptio/authenticator'
+  url 'https://github.com/heptio/authenticator/releases/download/v0.3.0/heptio-authenticator-aws_0.3.0_darwin_amd64'
+  version '0.3.0'
+  sha256 '9585d9af8d2f3b46b31649fb549978f4aedf2d62113fd42b57f9903ad71811c2'
+
+  depends_on :arch => :x86_64
+
+  def install
+    system "mv heptio-authenticator-aws_#{version}_darwin_amd64 heptio-authenticator-aws"
+    bin.install 'heptio-authenticator-aws'
+  end
+
+  test do
+    system "#{bin}/heptio-authenticator-aws"
+  end
+end
